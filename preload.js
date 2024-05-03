60351ca9d5b765d880b8e79caf96f96e93f0bc87
sharath R <114079961+sharathR143@users.noreply.github.com>
2024-05-03 16:29:47 +0530

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,16 +1,22 @@
-// const { contextBridge, ipcRenderer } = require("electron");
+/*
 
-// contextBridge.exposeInMainWorld("versions", {
-//   node: () => process.versions.node,
-//   chrome: () => process.versions.chrome,
-//   electron: () => process.versions.electron,
-//   getFiles: (title) => ipcRenderer.send("get-files", title),
-//   getFileList: (callback) => ipcRenderer.on("send-files", callback),
-//   sendFile: (fileName) => ipcRenderer.send("get-file", fileName),
-//   getFile: (callback) => ipcRenderer.on("send-file", callback),
-//   // we can also expose variables, not just functions
-// });
+const { contextBridge, ipcRenderer } = require("electron");
 
+contextBridge.exposeInMainWorld("versions", {
+  node: () => process.versions.node,
+  chrome: () => process.versions.chrome,
+  electron: () => process.versions.electron,
+  getFiles: (title) => ipcRenderer.send("get-files", title),
+  getFileList: (callback) => ipcRenderer.on("send-files", callback),
+  sendFile: (fileName) => ipcRenderer.send("get-file", fileName),
+  getFile: (callback) => ipcRenderer.on("send-file", callback),
+  // we can also expose variables, not just functions
+  sendUpdatedData: (jsondata, fileName) =>
+    ipcRenderer.send("get-updatedData", jsondata, fileName),
+  getUpdatedData: (callback) => ipcRenderer.on("send-updatedData", callback),
+});
+
+*/
 
 
 const { contextBridge, ipcRenderer } = require("electron");
