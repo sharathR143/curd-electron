6a974e57f1f80995b377321caf73e08b28577247
sharath R <114079961+sharathR143@users.noreply.github.com>
2024-05-03 06:55:17 +0530

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,3 +1,18 @@
+// const { contextBridge, ipcRenderer } = require("electron");
+
+// contextBridge.exposeInMainWorld("versions", {
+//   node: () => process.versions.node,
+//   chrome: () => process.versions.chrome,
+//   electron: () => process.versions.electron,
+//   getFiles: (title) => ipcRenderer.send("get-files", title),
+//   getFileList: (callback) => ipcRenderer.on("send-files", callback),
+//   sendFile: (fileName) => ipcRenderer.send("get-file", fileName),
+//   getFile: (callback) => ipcRenderer.on("send-file", callback),
+//   // we can also expose variables, not just functions
+// });
+
+
+
 const { contextBridge, ipcRenderer } = require("electron");
 
 contextBridge.exposeInMainWorld("versions", {
@@ -9,4 +24,7 @@ contextBridge.exposeInMainWorld("versions", {
   sendFile: (fileName) => ipcRenderer.send("get-file", fileName),
   getFile: (callback) => ipcRenderer.on("send-file", callback),
   // we can also expose variables, not just functions
+  sendUpdatedData: (jsondata, fileName) =>
+    ipcRenderer.send("get-updatedData", jsondata, fileName),
+  getUpdatedData: (callback) => ipcRenderer.on("send-updatedData", callback),
 });
